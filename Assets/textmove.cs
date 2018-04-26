6ecb8196b3ba797e0d8b780ba251b5d419801a50
Unknown <nemesismse@gmail.com>
2018-04-26 17:12:33 -0400

    Approaching finality

--- Assets/textmove.cs
+++ Assets/textmove.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class textmove : MonoBehaviour {
 
-	private float moveSpeed = 0.1f; //sets a var for move speed
+	private float moveSpeed = 0.07f; //sets a var for move speed
+	private float gametimer = 1;
 
 	// Use this for initialization
+
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		gametimer += 1;
+		if (gametimer >= 60*30){
+			moveSpeed = 0.1f;
+		}
 
-		transform.Translate (-moveSpeed, 0, 0); //sets text to move
+		if (gametimer >= 60*75){
+			moveSpeed = 0.12f;
+		}
+		transform.Translate (-moveSpeed * Time.deltaTime * 60, 0, 0); //sets text to move
 		
 	}
 }
